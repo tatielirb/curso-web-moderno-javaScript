8242819a92d59c129b7f93611bd9bfbbf5e6b2f4
Tatieli Ramos <tatielirb@gmail.com>
2020-07-19 20:12:18 -0300

    Add file exemple concat

---
+++ Secao_7_array/120_concat.js
@@ -0,0 +1,11 @@
+//concat
+const filhas = ['Ana', 'Bia']
+const filhos = ['Pedro', 'João']
+const todos = filhas.concat(filhos)
+console.log(todos, filhas, filhos)
+
+console.log([].concat([1, 2], [3, 4], 5, [[6, 7]]))
+
+//matris passado por parametro [[6, 7]]
+
+console.log(['a', 'b'].concat([1, 2], [3, 4], 5, [[6, 7]]))
