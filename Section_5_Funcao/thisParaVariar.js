b0bcec8706bbe2c599088a9997fef8bb443f53f3
Tatieli Ramos <tatielirb@gmail.com>
2020-07-26 16:46:09 -0300

    Add conceito this

--- Section_5_Funcao/thisParaVariar.js
+++ Section_5_Funcao/thisParaVariar.js
@@ -1,3 +1,6 @@
 //video 67
 //this para varias
-
+// This referencia o objeto atual da função
+//== é igual === restritamente igual
+//se tiver usado uma função arrow funcintion this não varia nunca
+//this é definido no momento que a função for definida
