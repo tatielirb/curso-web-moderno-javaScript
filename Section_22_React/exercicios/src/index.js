82deedfef183b8f2acdc241af7fad4540f467596
Tatieli Ramos <tatielirb@gmail.com>
2023-04-05 17:59:16 -0400

    componente com classe

--- Section_22_React/exercicios/src/index.js
+++ Section_22_React/exercicios/src/index.js
@@ -1,14 +1,27 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// import Primeiro from './componentes/Primeiro';
+// import { BoaTarde, BoaNoite } from './componentes/Multiplos';
+
+// ReactDOM.render(
+// <div>
+//   <Primeiro  nome="tati" idade="10"/>
+//   <BoaTarde nome="Ana"></BoaTarde>
+//   <BoaNoite nome="Bia"></BoaNoite>
+// </div>
+
+// , document.getElementById('root'));
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Primeiro from './componentes/Primeiro';
-import { BoaTarde, BoaNoite } from './componentes/Multiplos';
+import Saudacao from './componentes/Saudacao';
+
 
 ReactDOM.render(
 <div>
-  <Primeiro  nome="tati" idade="10"/>
-  <BoaTarde nome="Ana"></BoaTarde>
-  <BoaNoite nome="Bia"></BoaNoite>
+  <Saudacao tipo="Bom dia" nome="joão" />
 </div>
 
 , document.getElementById('root'));
