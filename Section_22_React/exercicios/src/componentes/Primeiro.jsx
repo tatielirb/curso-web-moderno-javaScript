ad03b41811f5973f4d211d58198ce41c373c70fd
Tatieli Ramos <tatielirb@gmail.com>
2023-04-04 20:08:37 -0400

    adicionando componentes mumtiplos

--- Section_22_React/exercicios/src/componentes/Primeiro.jsx
+++ Section_22_React/exercicios/src/componentes/Primeiro.jsx
@@ -1,3 +1,17 @@
-export default function() {
-  return "teste"
-}
+import React from "react";
+
+// export default props => [
+//   <h1 key="1">Bom dia {props.nome}</h1>
+//   <h2 key="2">>Bom dia {props.idade}</h2>
+// ]
+// export default props =>
+//   <React.Fragment>
+//     <h1>Bom dia {props.nome}</h1>
+//     <h2>Bom dia {props.idade}</h2>
+//   </React.Fragment>
+
+export default props =>
+  <div>
+    <h1>Bom dia {props.nome}</h1>
+    <h2>Bom dia {props.idade}</h2>
+  </div>
--- Section_22_React/exercicios/src/index.js
+++ Section_22_React/exercicios/src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import ReactDOM  from 'react';
+import ReactDOM from 'react-dom';
 
 import Primeiro from './componentes/Primeiro';
+import { BoaTarde, BoaNoite } from './componentes/Multiplos';
 
-ReactDOM.render(<Primeiro />, document.getElementById('root'));
+ReactDOM.render(
+<div>
+  <Primeiro  nome="tati" idade="10"/>
+  <BoaTarde nome="Ana"></BoaTarde>
+  <BoaNoite nome="Bia"></BoaNoite>
+</div>
+
+, document.getElementById('root'));
