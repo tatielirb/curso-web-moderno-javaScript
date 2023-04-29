e2a4a8b55ef3f709b5f805f3803c1b010ec9c414
Tatieli Ramos <tatielirb@gmail.com>
2023-04-29 19:17:31 -0400

    feat: adicionando layout no menu

--- Section_22_React/crud/frontend/src/components/template/Nav.jsx
+++ Section_22_React/crud/frontend/src/components/template/Nav.jsx
@@ -4,5 +4,13 @@ import React from 'react'
 
 export default props =>
     <aside className="menu-area">
-      Navegação
+      <nav className="menu">
+        {/*refatorar*/}
+        <a href="#/">
+          <i className='fa fa-home'></i> Inicio
+        </a>
+        <a href="#/users">
+          <i className='fa fa-users'></i> Usuários
+        </a>
+      </nav>
     </aside>
