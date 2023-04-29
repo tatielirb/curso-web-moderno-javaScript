8bbdf538707a2722066012bc38cbaaa0044968b7
Tatieli Ramos <tatielirb@gmail.com>
2023-04-29 19:43:23 -0400

    feat: adicionando estilo na main e ajuste com a home

---
+++ Section_22_React/crud/frontend/src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+  <Main icon="home" title="Inicio" subtitle="Segundo projeto do capito de React.">
+    <div className='display-4'>Bem vindo</div>
+    <p className='mb-0'>
+      Sistema para exemplificar a construção de um cadastro em React  
+    </p>   
+    
+  </Main>
