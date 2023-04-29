e09ac92fad4f284d1b15a12aa2968a44230642f2
Tatieli Ramos <tatielirb@gmail.com>
2023-04-29 19:00:54 -0400

    feat: adicionando estilização no footer

--- Section_22_React/crud/frontend/src/components/template/Footer.jsx
+++ Section_22_React/crud/frontend/src/components/template/Footer.jsx
@@ -1,9 +1,10 @@
 import './Footer.css'
 import React from 'react'
 
-
-
 export default props =>
    <footer className="footer">
-    Rodape
+    <span>
+      Desenvolvido com <i className='fa fa-heart text-danger'></i> pot
+      <strong> Tatieli Ramos</strong>
+    </span>
    </footer>
